3ebdce28f69923f3c880d84a26d327947d41cc56
andre.borba <dev_andre.filipe@outlook.com>
2023-04-09 04:16:51 -0300

    feat: unsigned integers

---
+++ src/basic_types/unsigned_integers.rs
@@ -0,0 +1,13 @@
+fn main() {
+  let unsigned_integer8: u8 = 255;
+  let unsigned_integer16: u16 = 65535;
+  let unsigned_integer32: u32 = 4294967295;
+  let unsigned_integer64: u64 = 18446744073709551615;
+  let unsigned_integer128: u128 = 340282366920938463463374607431768211455;
+
+  println!("Unsigned integer 8: {}", unsigned_integer8);
+  println!("Unsigned integer 16: {}", unsigned_integer16);
+  println!("Unsigned integer 32: {}", unsigned_integer32);
+  println!("Unsigned integer 64: {}", unsigned_integer64);
+  println!("Unsigned integer 128: {}", unsigned_integer128);
+}
