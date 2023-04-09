680f0ee072ea2b56b549fc3958f8512670a29698
andre.borba <dev_andre.filipe@outlook.com>
2023-04-09 04:17:41 -0300

    feat: platform specific integers

---
+++ src/basic_types/platform_specific_integers.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let specific_integeru: usize = 18446744073709551615;
+  let specific_integeri: isize = -9223372036854775808;
+
+  println!("Specific integer u: {}", specific_integeru);
+  println!("Specific integer i: {}", specific_integeri);
+}
