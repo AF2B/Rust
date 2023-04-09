9c11d9b383bfd9b6ded8c4d03af7691ce519b859
andre.borba <dev_andre.filipe@outlook.com>
2023-04-09 04:18:21 -0300

    feat: signed integers

---
+++ src/basic_types/signed_integers.rs
@@ -0,0 +1,13 @@
+fn main() {
+  let signed_integer8: i8 = -128;
+  let signed_integer16: i16 = -32768;
+  let signed_integer32: i32 = -2147483648;
+  let signed_integer64: i64 = -9223372036854775808;
+  let signed_integer128: i128 = -170141183460469231731687303715884105728;
+
+  println!("Signed integer 8: {}", signed_integer8);
+  println!("Signed integer 16: {}", signed_integer16);
+  println!("Signed integer 32: {}", signed_integer32);
+  println!("Signed integer 64: {}", signed_integer64);
+  println!("Signed integer 128: {}", signed_integer128);
+}
