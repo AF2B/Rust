baacd7e4a48918b7bcf03b97b0fcb9ebd901dc03
andre.borba <dev_andre.filipe@outlook.com>
2023-04-09 04:17:31 -0300

    feat: floating point type

---
+++ src/basic_types/floating_point_numbers.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let floating_point_number32: f32 = 3.14159265358979323846264338327950288;
+  let floating_point_number64: f64 = 3.14159265358979323846264338327950288;
+
+  println!("Floating point number 32: {}", floating_point_number32);
+  println!("Floating point number 64: {}", floating_point_number64);
+}
